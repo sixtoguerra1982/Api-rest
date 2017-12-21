1cf6c6190bc643e16aa830e8225a29ce9f3c4624
Felipe Loyola <felipe.l@funxperience.com>
2017-12-21 12:43:21 -0300

    Revert "Merged in richardbm1/backend-exam (pull request #1)"
    This reverts commit 2c47cabc9978c5d21434fb2a8ca336291ecc14f7.

--- blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from blog import models
-# Register your models here.
-
-admin.site.register(models.Publication)
-admin.site.register(models.Category)
-admin.site.register(models.Comment)
-admin.site.register(models.Like)
--- utils/base/fields.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from django.utils import timezone
-
-class DateTimeFieldWihTZ(serializers.DateTimeField):
-
-    def to_representation(self, value):
-        value = timezone.localtime(value)
-        return super(DateTimeFieldWihTZ, self).to_representation(value)
